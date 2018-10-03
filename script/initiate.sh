fe9c6e019f9548684587e76c71bafd5a32e3f15d
Karl Xu <karl.yogi.xu@gmail.com>
2018-10-03 22:06:36 +0800

    remove app_name from script

--- script/initiate.sh
+++ script/initiate.sh
@@ -5,14 +5,8 @@ then
     echo "Does not add parameter."; exit 1;
 fi
 
-if [ -z $ENV_APP_NAME ]
-then
-    echo "Did not set environment variable ENV_APP_NAME."; exit 1;
-fi
-
 IMAGE_NAME=heroku-docker
 PORT=8000
-APP_NAME=$ENV_APP_NAME
 
 if [ $1 = "build" ]
 then
@@ -37,12 +31,12 @@ then
 elif [ $1 = "push" ]
 then
 
-    heroku container:push -a ${APP_NAME} web
+    heroku container:push web
 
 elif [ $1 = "release" ]
 then
 
-    heroku container:release -a ${APP_NAME} web
+    heroku container:release  web
 
 else
     echo 'Does not recognize the command.'
